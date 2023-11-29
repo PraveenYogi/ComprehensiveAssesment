625103fc73918b38fb8c723b23bbf8df11cca728
PraveenYogi <72789570+PraveenYogi@users.noreply.github.com>
2023-11-29 12:30:47 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,13 @@
+package comprehensiveAssessment;
+
+public class Account {
+	long interest = 0;
+	public long get_Interest(long P, int T, float R) {
+	interest = (long) ((P * T * R) / 100);
+	return interest;
+	}
+	
+
+
+}
+
